7b8c6f9780e9394d3725455bce15ff47da6932a3
CaoYifan <g.xtz520@gmail.com>
2020-01-22 19:17:06 +0900

    JSONを使って天気情報を獲得

---
+++ app/src/main/java/local/hal/st31/ohtenki/WeatherInfoDialog.java
@@ -0,0 +1,30 @@
+package local.hal.st31.ohtenki;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class WeatherInfoDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        Bundle extras = getArguments();
+        String title = extras.getString("title");
+        String msg = extras.getString("msg");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage(msg);
+       builder.setPositiveButton("OK",new DialogButtonClickListener());
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    private class DialogButtonClickListener implements DialogInterface.OnClickListener{
+        @Override
+        public void onClick(DialogInterface dialog,int which){
+
+        }
+    }
+}
